034b50615f338d872be4f6a88293e06c471344dc
TMK33 <minhkhongthanh@gmail.com>
2024-07-04 11:41:00 +0200

    new commit

--- controllers/mitarbeiterController.js
+++ controllers/mitarbeiterController.js
@@ -11,4 +11,11 @@ exports.getAllMitarbeiter = (pool) => async (req, res) => {
 
 exports.addMitarbeiter = (pool) => async (req, res) => {
   // ... (Thêm logic xử lý cho việc thêm sinh viên)
+  try {
+    const { name, email, geburtsdatum, rolle } = req.body; // Lấy dữ liệu từ body request
+    const newMitarbeiter = await Mitarbeiter.add(pool, { name, email, geburtsdatum, rolle });
+    res.status(201).json(newMitarbeiter); // Trả về thông tin sinh viên mới với mã 201 Created
+} catch (err) {
+    res.status(500).json({ error: err.message });
+}
 };
--- models/mitarbeiter.js
+++ models/mitarbeiter.js
@@ -6,6 +6,12 @@ class Mitarbeiter {
   
     static async add(pool, mitarbeiterData) {
       // Thêm logic để chèn dữ liệu studentData vào bảng students
+      const { name, email, geburtsdatum, rolle } = mitarbeiterData;
+      const result = await pool.query(
+    'INSERT INTO mitarbeiter (name, email, geburtsdatum, rolle) VALUES ($1, $2, $3, $4) RETURNING *',
+    [name, email, geburtsdatum, rolle]
+  );
+      return result.rows[0];
     }
   }
   
