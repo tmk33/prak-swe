7c738b08bc7e71fea50ba130d485924e1c4b3329
Thanh Minh Khong <thanh.khong@stud.hn.de>
2024-07-07 00:12:52 +0200

    new commit

--- routes/sonderveranstaltungRoutes.js
+++ routes/sonderveranstaltungRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const raumController = require('../controllers/sonderveranstaltungController');
+const authenticateAdmin = require('../middleware/auth');
+
 
 module.exports = (pool) => {
   const router = express.Router();
 
   router.get('/', raumController.getAllSonderveranstaltung(pool));
-  router.post('/', raumController.addSonderveranstaltung(pool));
+  router.post('/', authenticateAdmin.authenticateAdmin, raumController.addSonderveranstaltung(pool));
 
   return router;
 };
